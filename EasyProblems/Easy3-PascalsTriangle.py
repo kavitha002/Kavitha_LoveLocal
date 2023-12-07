25cc84886f69ab1ea9dafeddace63c81d8b09818
kavitha002 <142476697+kavitha002@users.noreply.github.com>
2023-12-07 11:24:46 +0530

    Update Easy3-PascalsTriangle.py

--- EasyProblems/Easy3-PascalsTriangle.py
+++ EasyProblems/Easy3-PascalsTriangle.py
@@ -12,25 +12,29 @@ Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 '''
 #code ->
 
-def generate(self, numRows):
-        List=[] 
-        list_0=[1]
-        list_1=[1,1]
-        List.append(list_0)
-        List.append(list_1)
-        if numRows == 1:
-            return [[1]]
-        if numRows == 2:
-            return [[1],[1,1]]
-        if numRows>=3:
-            for i in range(2,numRows): 
-                list_i=[1] 
-                for j in range (1,i):
-                    list_i.append(List[i-1][j-1]+List[i-1][j])
-                list_i.append(1)
-                List.append(list_i)
-    
-        return List
+def generate(numRows):
+    List = [] 
+    list_0 = [1]
+    list_1 = [1, 1]
+    List.append(list_0)
+    List.append(list_1)
+
+    if numRows == 1:
+        return [[1]]
+    if numRows == 2:
+        return [[1], [1, 1]]
+    if numRows >= 3:
+        for i in range(2, numRows): 
+            list_i = [1] 
+            for j in range(1, i):
+                list_i.append(List[i - 1][j - 1] + List[i - 1][j])
+            list_i.append(1)
+            List.append(list_i)
+
+    return List
+
+result = generate(5)
+print(result)
     
 '''
  TRACING:
